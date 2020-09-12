187e94f69fc91d36ef04d7529c5c6cfe66028d8f
Gastón Muñoz <gaston.munoz@vipealo.com>
2020-09-12 06:49:08 -0300

    [TEST] Corrigiendo pruebas

--- spec/coTestSpec.js
+++ spec/coTestSpec.js
@@ -8,8 +8,8 @@ describe("Co Test", function() {
 
   it("should foo", function() {
     const coTest = new CarInsurance([ new Product("foo", 0, 0) ]);
-    const products = coTest.updatePrice();
-    expect(products[0].name).equal("fixme");
+    const products = coTest.updateProducts();
+    expect(products[0].name).equal("foo");
   });
 
 });
